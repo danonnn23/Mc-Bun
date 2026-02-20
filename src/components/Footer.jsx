a96c4e136d4ea040f48fdd17a5bf7afd2a04abbc
danonnn23 <tolochkodan.work@gmail.com>
2026-02-20 22:37:24 +0200

    fixed footer blocks width and changed website logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,25 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <div className="footer-brand">
-          <h2>McBun</h2>
-          <p>Feeding heroes since 2026</p>
+        <div className="footer-brand footer-block">
+          <div>
+            <h2>McBun</h2>
+            <p>Feeding heroes since 2026</p>
+          </div>
         </div>
-        <nav className="footer-nav">
-          <NavLink to="/home">HOME</NavLink>
-          <NavLink to="/menu">MENU</NavLink>
-          <NavLink to="/about">ABOUT</NavLink>
-          <NavLink to="/delivery">DELIVERY</NavLink>
-        </nav>
-        <div className="footer-bottom">
-          <p>MADE WITH POW! BY Danylo Tolochko</p>
-          <p>© 2026 McBun Comics. All rights reserved</p>
+        <div className='footer-block'>
+          <nav className="footer-nav">
+            <NavLink to="/home">HOME</NavLink>
+            <NavLink to="/menu">MENU</NavLink>
+            <NavLink to="/about">ABOUT</NavLink>
+            <NavLink to="/delivery">DELIVERY</NavLink>
+          </nav>
+        </div>
+        <div className="footer-bottom footer-block">
+          <div>
+            <p>MADE WITH POW! BY Danylo Tolochko</p>
+            <p>© 2026 McBun Comics. All rights reserved</p>
+          </div>
         </div>
       </div>
     </footer>
